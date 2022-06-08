525a64efaa4e48d0652a2f1edfe47c3214465b38
juancito.eth <12957692+juanscolari@users.noreply.github.com>
2022-06-08 14:18:38 -0300

    feat: implement status endpoint (#61)

---
+++ src/controllers/handlers/status-handler.ts
@@ -0,0 +1,12 @@
+import { HandlerContextWithPath } from '../../types'
+
+// handlers arguments only type what they need, to make unit testing easier
+export async function statusHandler(
+  _context: Pick<HandlerContextWithPath<'metrics', '/status'>, 'url' | 'components'>
+) {
+  return {
+    body: {
+      commitHash: process.env.COMMIT_HASH
+    }
+  }
+}
