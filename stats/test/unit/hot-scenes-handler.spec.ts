211dce472a6ddce726e93462bfd5e0f7731ce7ce
Hugo Arregui <hugo@decentraland.org>
2023-07-05 16:13:04 -0300

    Save Wed Jul  5 04:13:04 PM -03 2023

--- stats/test/unit/hot-scenes-handler.spec.ts
+++ stats/test/unit/hot-scenes-handler.spec.ts
@@ -4,42 +4,6 @@ import { createStatsComponent } from '../../src/adapters/stats'
 
 describe('hot-scenes-handler-unit', () => {
   it('ok', async () => {
-    const parcels: CatalystParcelsInfo[] = [
-      {
-        realmName: 'catalyst-1',
-        url: 'https://catalyst-1',
-        parcels: [
-          {
-            peersCount: 10,
-            parcel: {
-              x: 10,
-              y: 10
-            }
-          },
-          {
-            peersCount: 12,
-            parcel: {
-              x: 11,
-              y: 10
-            }
-          }
-        ]
-      },
-      {
-        realmName: 'catalyst-2',
-        url: 'https://catalyst-2',
-        parcels: [
-          {
-            peersCount: 5,
-            parcel: {
-              x: 10,
-              y: 10
-            }
-          }
-        ]
-      }
-    ]
-
     const scenes = {
       '10,10': {
         id: 1,
@@ -63,7 +27,29 @@ describe('hot-scenes-handler-unit', () => {
     }
 
     const stats = createStatsComponent()
-    stats.onCatalystsParcelsInfo({ time: 0, info: parcels })
+
+    for (let i = 0; i < 10; i++) {
+      const id = `${i}`
+      const data = {
+        time: Date.now(),
+        address: id,
+        x: 10 * 16,
+        y: 0,
+        z: 10 * 16
+      }
+      stats.onPeerUpdated(id, data)
+    }
+    for (let i = 0; i < 12; i++) {
+      const id = `${10 + i}`
+      const data = {
+        time: Date.now(),
+        address: id,
+        x: 10 * 16,
+        y: 0,
+        z: 11 * 16
+      }
+      stats.onPeerUpdated(id, data)
+    }
     const content = {
       fetchScenes: async (tiles: string[]) => {
         const result: Entity[] = []
@@ -78,16 +64,15 @@ describe('hot-scenes-handler-unit', () => {
     }
 
     const url = new URL('https://aggregator.com/hot-scenes')
-    const { headers, body } = await hotScenesHandler({ url, components: { stats, content } })
+    const { body } = await hotScenesHandler({ url, components: { stats, content } })
 
     const result: HotSceneInfo[] = body
-    expect(headers['Last-Modified']).toEqual('Thu, 01 Jan 1970 00:00:00 GMT')
 
     expect(result).toHaveLength(1)
     expect(result[0].id).toEqual(1)
     expect(result[0].name).toEqual('test')
     expect(result[0].baseCoords).toEqual([10, 10])
-    expect(result[0].usersTotalCount).toEqual(15)
+    expect(result[0].usersTotalCount).toEqual(22)
     expect(result[0].parcels).toEqual([
       [10, 10],
       [10, 11]
@@ -96,36 +81,5 @@ describe('hot-scenes-handler-unit', () => {
     expect(result[0].projectId).toEqual('project-id')
     expect(result[0].creator).toEqual('creator')
     expect(result[0].description).toEqual('a test')
-    expect(result[0].realms).toEqual([
-      {
-        serverName: 'catalyst-1',
-        url: 'https://catalyst-1',
-        usersCount: 10,
-        userParcels: [
-          [10, 10],
-          [10, 10],
-          [10, 10],
-          [10, 10],
-          [10, 10],
-          [10, 10],
-          [10, 10],
-          [10, 10],
-          [10, 10],
-          [10, 10]
-        ]
-      },
-      {
-        serverName: 'catalyst-2',
-        url: 'https://catalyst-2',
-        usersCount: 5,
-        userParcels: [
-          [10, 10],
-          [10, 10],
-          [10, 10],
-          [10, 10],
-          [10, 10]
-        ]
-      }
-    ])
   })
 })
